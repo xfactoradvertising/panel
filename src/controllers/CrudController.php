16ff918e77ab3ac7bd18c8bafbd0e187c760b1ed
Josh Guice <jguice@gmail.com>
2017-04-25 17:34:38 -0700

    feat(table): limit options to just show

--- src/controllers/CrudController.php
+++ src/controllers/CrudController.php
@@ -49,7 +49,8 @@ class CrudController extends Controller
 
     public function addStylesToGrid($orderByColumn = 'id', $paginateCount = 10)
     {
-        $this->grid->edit('edit', trans('panel::fields.edit'), 'show|modify|delete');
+//        $this->grid->edit('edit', trans('panel::fields.edit'), 'show|modify|delete');
+        $this->grid->edit('edit', '', 'show');
 
         $this->grid->orderBy($orderByColumn, 'desc');
         $this->grid->paginate($paginateCount);        

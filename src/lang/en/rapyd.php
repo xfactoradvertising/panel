a547127282f122ff32ef6e02afa4b48773f0ab1b
Josh Guice <jguice@gmail.com>
2017-06-22 20:47:09 -0700

    fix(delete): add localized text

--- src/lang/en/rapyd.php
+++ src/lang/en/rapyd.php
@@ -2,4 +2,8 @@
 
 return array(
     'empty_list' => 'No results found',
+    'conf_delete' => 'Are you sure you want to permanently delete this record?',
+    'undo' => 'Cancel',
+    'delete' => 'Delete',
+    'deleted' => 'Deleted.'
 );
